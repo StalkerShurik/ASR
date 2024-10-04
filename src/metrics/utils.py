93160e4d61287924e8671075f79ae26fadaf9029
Alexander Gaponov <agaponov04@gmail.com.com>
2024-10-04 17:54:46 +0300

    metrics untested

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -1,13 +1,15 @@
+import fastwer
+
 # Based on seminar materials
 
 # Don't forget to support cases when target_text == ''
 
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    # print(target_text.shape, predicted_text.shape)
+    return fastwer.score(predicted_text, target_text, char_level=True)
 
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    # print(target_text.shape, predicted_text.shape)
+    return fastwer.score(predicted_text, target_text)
