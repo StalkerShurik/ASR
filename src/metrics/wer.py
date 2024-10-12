8801db81cc21a706cd2fe1780de9caafbf99d50a
Alexander Gaponov <agaponov04@gmail.com.com>
2024-10-12 20:57:09 +0300

    updated metrics with bs

--- src/metrics/wer.py
+++ src/metrics/wer.py
@@ -6,9 +6,27 @@ from torch import Tensor
 from src.metrics.base_metric import BaseMetric
 from src.metrics.utils import calc_wer
 
-# TODO beam search / LM versions
+# TODO LM versions
 # Note: they can be written in a pretty way
 # Note 2: overall metric design can be significantly improved
+
+
+class BeamSearchWERMetric(BaseMetric):
+    def __init__(self, text_encoder, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.text_encoder = text_encoder
+
+    def __call__(
+        self, log_probs: Tensor, log_probs_length: Tensor, text: List[str], **kwargs
+    ):
+        cers = []
+
+        lengths = log_probs_length.detach().numpy()
+        for log_prob_vec, length, target_text in zip(log_probs.cpu(), lengths, text):
+            target_text = self.text_encoder.normalize_text(target_text)
+            pred_text = self.text_encoder.ctc_decode_beam_search(log_prob_vec[:length])
+            cers.append(calc_wer(target_text, pred_text))
+        return sum(cers) / len(cers)
 
 
 class ArgmaxWERMetric(BaseMetric):
