669de51f64b0506e6c309ddf8997587bad5fcfd7
Alexander Gaponov <agaponov04@gmail.com.com>
2024-10-13 22:20:41 +0300

    metrcis/cer

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -1,2 +1,6 @@
-from src.metrics.cer import ArgmaxCERMetric, BeamSearchCERMetric
+from src.metrics.cer import (
+    ArgmaxCERMetric,
+    BeamSearchCERMetric,
+    CustomeBeamSearchCERMetric,
+)
 from src.metrics.wer import ArgmaxWERMetric, BeamSearchWERMetric
